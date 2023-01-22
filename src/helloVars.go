5e9bd3363c8461d2af1196fcb31b4b26dc3bcd11
0Left <leftamail@gmail.com>
2023-01-22 14:39:33 -0300

    Deixando bonitin já que o prettier n ta indo

--- src/helloVars.go
+++ src/helloVars.go
@@ -9,23 +9,26 @@ import (
 
 //toda pagina main precisa de sua função correspondente
 func main(){
-	//declaração padrão(var + nome + tipo)
-	var justDeclared string;
-	//pode adicionar o valor para atribuir (= valor)
-	var nome string = "Pedro";
+
+	//declaração longa + atribuição
 	var idadeInt int = 23;
 	var nota float32 = 1.0;
+
+	//declaração longa (var + nome + tipo)
+	var justDeclared string;
+
+	//declaração com auto atribuiçãol de tipos (usar com tipos faceis)
+	var nome = "Pedro";
+
 	//declaração dinamica (nome := valor)
 	idadeDin := 10
-	fmt.Println("Ola justDeclared!",justDeclared);
-	fmt.Println("Ola nome!",nome);
-	fmt.Println("Ola idadeInt!",idadeInt);
-	fmt.Println("Ola nota!",nota);
-	fmt.Println("Ola idadeDin!",idadeDin);
 
-	fmt.Println("My type is",reflect.TypeOf(justDeclared));
-	fmt.Println("My type is",reflect.TypeOf(idadeInt));
-	fmt.Println("My type is",reflect.TypeOf(nota));
+	//printa tudo pra ver, com os tipos
+	fmt.Println("Ola justDeclared!",justDeclared,reflect.TypeOf(justDeclared));
+	fmt.Println("Ola nome!",nome,reflect.TypeOf(nome));
+	fmt.Println("Ola idadeInt!",idadeInt,reflect.TypeOf(idadeInt));
+	fmt.Println("Ola nota!",nota,reflect.TypeOf(nota));
+	fmt.Println("Ola idadeDin!",idadeDin,reflect.TypeOf(idadeDin));
 }
 
 //; não é obrigatório mas ja é quase um reflexo meu, malz.
